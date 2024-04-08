3b1ef93f7252e93621c31227d8b0dd8c3caefa38
PerpetuumG <perpetuumg@gmail.com>
2024-04-08 15:24:14 +0300

    add Banner

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 import { logo } from '../../assets/index';
 import { navLinksdata } from '../../constants';
 
 const Navbar = () => {
   return (
-    <div className={'w-full h-20 mx-auto flex justify-between items-center font-titleFont'}>
+    <div
+      className={
+        'w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600'
+      }
+    >
       <div>
         <img src={logo} alt='logo' />
       </div>
@@ -18,7 +23,16 @@ const Navbar = () => {
                 'text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300'
               }
             >
-              {title}
+              <Link
+                activeClass={'active'}
+                to={link}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                {title}
+              </Link>
             </li>
           ))}
         </ul>
