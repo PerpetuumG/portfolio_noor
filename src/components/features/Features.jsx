484e9fbea9d8874cf9dc8967d4f7434ccdcef55f
PerpetuumG <perpetuumg@gmail.com>
2024-04-08 15:47:33 +0300

    add data.js

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import Title from "../layouts/Title";
+import Title from '../layouts/Title';
+import Card from './Card';
 
 const Features = () => {
   return (
-    <section
-      id={'features'}
-      className={'w-full h-[800px] py-20 flex border-b-[1px] border-b-black'}
-    >
-    <Title title={'Features'} des={'What I do'} />
+    <section id={'features'} className={'w-full py-20 border-b-[1px] border-b-black'}>
+      <Title title={'Features'} des={'What I do'} />
+
+      <div className={'grid grid-cols-3 gap-20'}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </section>
   );
 };
