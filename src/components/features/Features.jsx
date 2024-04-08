dc93f3af4c8412af268c4572b147db5bf46e7335
PerpetuumG <perpetuumg@gmail.com>
2024-04-08 16:40:48 +0300

    add Card

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Title from '../layouts/Title';
 import Card from './Card';
+import { featuresData } from '../../data/data';
 
 const Features = () => {
   return (
@@ -8,12 +9,9 @@ const Features = () => {
       <Title title={'Features'} des={'What I do'} />
 
       <div className={'grid grid-cols-3 gap-20'}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {featuresData.map(({ id, icon, title, des }) => (
+          <Card id={id} icon={icon} title={title} des={des} />
+        ))}
       </div>
     </section>
   );
