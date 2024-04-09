4ad24290a7f9fa6bd406ea3ceef2b0a65ac96525
PerpetuumG <perpetuumg@gmail.com>
2024-04-09 16:06:08 +0300

    add Testimonial cards

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Banner from './components/banner/Banner';
 import Features from './components/features/Features';
 import Projects from './components/projects/Projects';
 import Resume from './components/resume/Resume';
+import Testimonial from "./components/tesimonial/Testimonial";
 
 function App() {
   return (
-    <div className={'w-full h-auto bg-bodyColor text-lightText'}>
-      <div className={'max-w-screen-2xl mx-auto px-16'}>
-        <Navbar />
+    <div className={'w-full h-auto bg-bodyColor text-lightText px-4'}>
+      <Navbar />
+      <div className={'max-w-screen-xl mx-auto'}>
         <Banner />
         <Features />
         <Projects />
         <Resume />
+        <Testimonial/>
       </div>
     </div>
   );
